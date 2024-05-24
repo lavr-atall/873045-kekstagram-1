d203249ed69d050b446bd9decd6c5746d65d8313
lavr-atall <l.a.v.r@mail.ru>
2024-05-24 11:05:17 +0300

    внес корректировки

--- js/main.js
+++ js/main.js
@@ -70,23 +70,22 @@ for (let i = 0; i < 15; i++) {
   const userPhotoParams = {
     id: idAndUrl,
     url: `photos/${idAndUrl}.jpg`,
-    description: photoDescriprion[getRandomDigits(1, photoDescriprion.length - 1)],
+    description: photoDescriprion[getRandomDigits(0, photoDescriprion.length - 1)],
     likes: getRandomDigits(1, 50),
-    comments: [
-      {
-        id: getRandomInteger(1, 25),
-        avatar: `img/avatar-${getRandomDigits(1, 6)}.svg`,
-        message: commentsMessage[getRandomDigits(0, commentsMessage.length - 1)],
-        name: userNames[getRandomDigits(0, userNames.length - 1)]
-      },
-      {
-        id: getRandomInteger(1, 25),
-        avatar: `img/avatar-${getRandomDigits(1, 6)}.svg`,
-        message: commentsMessage[getRandomDigits(0, commentsMessage.length - 1)],
-        name: userNames[getRandomDigits(0, userNames.length - 1)]
-      }
-    ]
+    comments: [],
   };
+
+  const generateCommentsQty = getRandomInteger(1, 10);
+
+  for (let j = 0; j < generateCommentsQty; j++) {
+    const singleComment = {
+      id: getRandomInteger(1, 25),
+      avatar: `img/avatar-${getRandomDigits(1, 6)}.svg`,
+      message: commentsMessage[getRandomDigits(0, commentsMessage.length - 1)],
+      name: userNames[getRandomDigits(0, userNames.length - 1)],
+    };
+    userPhotoParams.comments.push(singleComment);
+  }
+
   mockPhotos.push(userPhotoParams);
 }
-
