aab6ad5cb279ba9a2d59de9ddf0f31f04cd25131
lavr-atall <l.a.v.r@mail.ru>
2024-05-24 11:13:35 +0300

    разбивка на модули

--- js/mock-photos-generator.js
+++ js/mock-photos-generator.js
@@ -3,31 +3,30 @@ const mockPhotos = [];
 
 import { getRandomDigits, getRandomInteger, generatePhotoIdAndUrl } from './generator.js';
 import { userNames, commentsMessage, photoDescriprion } from './data.js';
+
 for (let i = 0; i < 15; i++) {
   const idAndUrl = generatePhotoIdAndUrl();
   const userPhotoParams = {
     id: idAndUrl,
     url: `photos/${idAndUrl}.jpg`,
-    description: photoDescriprion[getRandomDigits(1, photoDescriprion.length - 1)],
+    description: photoDescriprion[getRandomDigits(0, photoDescriprion.length - 1)],
     likes: getRandomDigits(1, 50),
-    comments: [
-      {
-        id: getRandomInteger(1, 25),
-        avatar: `img/avatar-${getRandomDigits(1, 6)}.svg`,
-        message: commentsMessage[getRandomDigits(0, commentsMessage.length - 1)],
-        name: userNames[getRandomDigits(0, userNames.length - 1)]
-      },
-      {
-        id: getRandomInteger(1, 25),
-        avatar: `img/avatar-${getRandomDigits(1, 6)}.svg`,
-        message: commentsMessage[getRandomDigits(0, commentsMessage.length - 1)],
-        name: userNames[getRandomDigits(0, userNames.length - 1)]
-      }
-    ]
+    comments: [],
   };
 
-  mockPhotos.push(userPhotoParams);
+  const generateCommentsQty = getRandomInteger(1, 10);
 
+  for (let j = 0; j < generateCommentsQty; j++) {
+    const singleComment = {
+      id: getRandomInteger(1, 25),
+      avatar: `img/avatar-${getRandomDigits(1, 6)}.svg`,
+      message: commentsMessage[getRandomDigits(0, commentsMessage.length - 1)],
+      name: userNames[getRandomDigits(0, userNames.length - 1)],
+    };
+    userPhotoParams.comments.push(singleComment);
+  }
+
+  mockPhotos.push(userPhotoParams);
 }
 
 export { mockPhotos };
