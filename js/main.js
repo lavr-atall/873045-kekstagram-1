fe43d81e9f470c243a457e4febf430c6af3ca4de
lavr-atall <l.a.v.r@mail.ru>
2024-07-30 11:41:05 +0300

    добавлена валидация формы

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 import { renderThumbnails } from './render-picture-thumbnails.js';
 import './popup.js';
-window.log (renderThumbnails);
+//window.log (renderThumbnails);
+import './form-validation.js';
